c1e8053b6994e32732206c436752e59727f2a8bd
scrazzz <70033559+scrazzz@users.noreply.github.com>
2021-09-28 12:19:36 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,4 @@
 rootProject.name = "AliucordPlugins"
 
-include(":HelloWorld")
-project(":HelloWorld").projectDir = File("./HelloWorld")
-
 include(":GoogleIt")
 project(":GoogleIt").projectDir = File("./GoogleIt")
