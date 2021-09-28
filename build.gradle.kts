d947a49277120672b91658d83bcf5e04b57c149e
scrazzz <scrazzz@github.com>
2021-09-28 11:25:16 +0300

    fix stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.aliucord.gradle.AliucordExtension
 import com.android.build.gradle.BaseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -9,7 +10,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("com.github.Aliucord:gradle:main-SNAPSHOT")
+        classpath("com.github.Aliucord:gradle:resources-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     }
 }
 
@@ -21,18 +23,24 @@ allprojects {
     }
 }
 
-fun Project.aliucord(configuration: AliucordExtension.() -> Unit) = extensions.getByName<AliucordExtension>("aliucord").configuration()
+fun Project.aliucord(
+        configuration: AliucordExtension.() -> Unit
+) = extensions.getByName<AliucordExtension>("aliucord").configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(
+        configuration: BaseExtension.() -> Unit
+) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "com.aliucord.gradle")
+    apply(plugin = "kotlin-android")
 
     aliucord {
-        author("scruz", 794527403580981248L)
-        updateUrl.set("https://raw.githubusercontent.com/scrazzz/AliucordPlugins/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/scrazzz/AliucordPlugins/builds/%s.zip")
+        val baseGithubUrl = "https://raw.githubusercontent.com/scrazzz/AliucordPlugins"
+        author("scrzu", 794527403580981248L)
+        updateUrl.set("$baseGithubUrl/builds/updater.json")
+        buildUrl.set("$baseGithubUrl/builds/%s.zip")
     }
 
     android {
@@ -40,25 +48,29 @@ subprojects {
 
         defaultConfig {
             minSdk = 24
-            targetSdk = 30
+            targetSdk= 30
+            versionCode = 1
+            versionName = "1.0"
         }
 
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_11
             targetCompatibility = JavaVersion.VERSION_11
         }
+
+        tasks.withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
     }
 
     dependencies {
-        val discord by configurations
-        val implementation by configurations
+        "discord"("com.discord:discord:aliucord-SNAPSHOT")
+        "implementation"("com.github.Aliucord:Aliucord:main-SNAPSHOT")
 
-        discord("com.discord:discord:aliucord-SNAPSHOT")
-        implementation("com.github.Aliucord:Aliucord:main-SNAPSHOT")
-
-        implementation("androidx.appcompat:appcompat:1.3.1")
-        implementation("com.google.android.material:material:1.4.0")
-        implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+        "implementation"("androidx.appcompat:appcompat:1.3.1")
+        "implementation"("com.google.android.material:material:1.4.0")
     }
 }
 
