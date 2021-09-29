8917cb551be788b33b8d7693133d9c22e7410753
scrazzz <officialavinash.2004@gmail.com>
2021-09-29 09:43:43 +0300

    New plugin build.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,6 @@ rootProject.name = "AliucordPlugins"
 
 include(":GoogleIt")
 project(":GoogleIt").projectDir = File("./GoogleIt")
+
+include(":Capitalize")
+project(":Capitalize").projectDir = File("./Capitalize")
