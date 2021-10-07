bc3f6a1da59e4734d14ac1c34d8fee3e351ed426
scrazzz <scrazzz@users.noreply.github.com>
2021-10-07 20:40:51 +0300

    new plugin

---
+++ NekosLife/build.gradle.kts
@@ -0,0 +1,2 @@
+version = "1.0.0"
+description = "Get images/gifs from nekos.life API"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,6 @@ project(":GoogleIt").projectDir = File("./GoogleIt")
 
 include(":Capitalize")
 project(":Capitalize").projectDir = File("./Capitalize")
+
+include(":NekosLife")
+project(":NekosLife").projectDir = File("./NekosLife")
