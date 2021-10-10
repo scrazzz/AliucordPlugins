a4cff1e8b30c03a35e9a814919209b8baef53e81
scrazzz <officialavinash.2004@gmail.com>
2021-10-10 19:27:25 +0300

    Fix NekosLife

--- NekosLife/build.gradle.kts
+++ NekosLife/build.gradle.kts
@@ -1,15 +1,18 @@
-version = "1.2.0"
+version = "1.2.1"
 description = "Get images/gifs from nekos.life API"
 
 aliucord.changelog.set(
         """
 
+        # 1.2.1
+        * Fixes "category" not showing up
+
         # 1.2.0
         * Support v96
-        
+
         # 1.1.0
         * Clyde now embeds images/gifs.
         * Plugin sends message as Clyde only in Aliucord server if `send: true`.
-        
+
         """.trimIndent()
 )
