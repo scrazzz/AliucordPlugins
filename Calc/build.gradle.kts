e590dc59bdf7fa10d38129371ae359be4d62e1ef
scrazzz <officialavinash.2004@gmail.com>
2021-10-19 06:55:24 +0300

    New plugin - Calc

---
+++ Calc/build.gradle.kts
@@ -0,0 +1,11 @@
+version = "1.0.0"
+description = "A simple calculator to do simple calculations"
+
+aliucord.changelog.set(
+        """
+
+        # 1.0.0
+        * Initial release
+
+        """.trimIndent()
+)
