5b6af18b49d0005cb7e009b3fcc576b0d10c7cf2
scrazzz <officialavinash.2004@gmail.com>
2021-10-22 16:26:55 +0300

    Add Calc Plugin - Unfinished

--- Calc/src/main/java/com/aliucord/plugins/Calc.kt
+++ Calc/src/main/java/com/aliucord/plugins/Calc.kt
@@ -4,79 +4,72 @@ import android.content.Context
 
 import com.aliucord.Utils
 import com.aliucord.Logger
-import com.aliucord.api.CommandsAPI
+import com.aliucord.api.CommandsAPI.CommandResult
 import com.aliucord.entities.Plugin
-import com.aliucord.Utils.createCommandChoice
+import com.aliucord.Http
 import com.aliucord.annotations.AliucordPlugin
 
 import com.discord.api.commands.ApplicationCommandType
 
+class Result(val queryresult: QueryResult) {
+    class QueryResult(val inputstring: String, val pods: List<Pod>)
+    class Pod(val subpods: List<SubPods>)
+    class SubPods(val plaintext: String)
+}
 
-private fun add(n1: Long, n2: Long) = n1 + n2
-private fun sub(n1: Long, n2: Long) = n1 - n2
-private fun mul(n1: Long, n2: Long) = n1 * n2
-private fun div(n1: Long, n2: Long) = n1 / n2
-/*{
-    if (n2 == 0) { return "You fool" }
-    else { n1 / n2 }
-}*/
+private fun fmtSend(data: Result): String {
+    return StringBuilder()
+            .append("**Input:**\n`${data.queryresult.inputstring}`\n")
+            .append("**Output:**\n`${data.queryresult.pods[1].subpods[0].plaintext}`")
+            .toString()
+}
 
 @AliucordPlugin
 class Calc : Plugin() {
     private val LOGGER = Logger("Calc")
 
     override fun start(ctx: Context) {
-        val opChoices = listOf(
-                createCommandChoice("+", "+"),
-                createCommandChoice("-", "-"),
-                createCommandChoice("*", "*"),
-                createCommandChoice("/", "/")
+        val args = listOf(
+                Utils.createCommandOption(
+                        ApplicationCommandType.STRING,
+                        "input",
+                        "Input a question",
+                        required = true),
+                Utils.createCommandOption(
+                        ApplicationCommandType.BOOLEAN,
+                        "send",
+                        "Send to chat",
+                        required = false)
         )
-        val args = listOf(Utils.createCommandOption(
-                ApplicationCommandType.INTEGER,
-                "num1",
-                "Enter first number",
-                null,
-                true
-        ), Utils.createCommandOption(
-                ApplicationCommandType.STRING,
-                "operation",
-                "Enter the operation",
-                null,
-                true,
-                default = false,
-                channelTypes = emptyList(),
-                choices = opChoices
-        ), Utils.createCommandOption(
-                ApplicationCommandType.INTEGER,
-                "num2",
-                "Enter the second number",
-                null,
-                true
-        ))
 
         commands.registerCommand(
                 "calc",
-                "Calculate simple numbers",
+                "Calculate your answer using wolfram API",
                 args
         ) { ctx -> try {
-            val n1 = ctx.getRequiredLong("num1")
-            val op = ctx.getRequiredString("operation")
-            val n2 = ctx.getRequiredLong("num2")
-            var result: String
-
-            when (op) {
-                "+" -> CommandsAPI.CommandResult(add(n1, n2).toString())
-                "-" -> CommandsAPI.CommandResult(sub(n1, n2).toString())
-                "*" -> CommandsAPI.CommandResult(mul(n1, n2).toString())
-                "/" -> CommandsAPI.CommandResult(div(n1, n2).toString())
-                else -> CommandsAPI.CommandResult("Invalid operator")
+            val url = Http.QueryBuilder("https://api.wolframalpha.com/v2/query")
+                    .append("input", ctx.getRequiredString("input"))
+                    .append("format", "plaintext")
+                    .append("output", "JSON")
+                    .append("appid", "no")
+                    .toString()
+            try {
+                val result = Http.Request(url, "GET").execute().json(Result::class.java)
+                val toSend = fmtSend(result)
+                CommandResult(toSend)
+            } catch (t: Throwable) {
+                val resultAgain = Http.Request(url, "GET").execute().text()
+                if ("Invalid appid" in resultAgain) {
+                    CommandResult("An error occurred: `Invalid API Key`\nCheck Debug Logs for info.")
+                } else {
+                    CommandResult("An error occurred. Check Debug Logs.")
+                }
             }
-        } catch (t: Throwable) {
+        }
+        catch (t: Throwable) {
             LOGGER.error(t)
-            CommandsAPI.CommandResult("An error occurred.", null, false)
-        }
-        }
+            CommandResult("An error occurred.", null, false)
+        } }
     }
 
     override fun stop(ctx: Context) = commands.unregisterAll()
