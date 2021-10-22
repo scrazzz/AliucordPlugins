6a034bc351c5789baba464b0e0d107439039215a
scrazzz <scrazzz@github.com>
2021-10-22 16:28:45 +0300

    New plugin - Ip

---
+++ Ip/build.gradle.kts
@@ -0,0 +1,11 @@
+version = "1.0.0"
+description = "Get details about IPv4/IPv6 address or a domain name."
+
+aliucord.changelog.set(
+        """
+
+        # 1.0.0
+        * Initial release
+
+        """.trimIndent()
+)
