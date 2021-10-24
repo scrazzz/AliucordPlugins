6a3d7fa48dec2ee033b26f8fb42a01c387b913dc
scrazzz <officialavinash.2004@gmail.com>
2021-10-24 17:25:25 +0300

    Update IP plugin

--- Ip/build.gradle.kts
+++ Ip/build.gradle.kts
@@ -4,6 +4,11 @@ description = "Get details about IPv4/IPv6 address or a domain name."
 aliucord.changelog.set(
         """
 
+        # 2.0.0
+        * Added new API which gives more details.
+        * Added "send" option.
+        * Note: New API has a limit of 10,000 requests per IP per month.
+
         # 1.0.0
         * Initial release
 
