844db859fc0e929a85f2b4fbe4a7f23ade99e28c
scrazzz <scrazzz@users.noreply.github.com>
2021-10-24 17:25:25 +0300

    Update IP plugin

--- Ip/src/main/java/com/aliucord/plugins/Ip.kt
+++ Ip/src/main/java/com/aliucord/plugins/Ip.kt
@@ -10,8 +10,8 @@ import com.aliucord.entities.Plugin
 import com.aliucord.annotations.AliucordPlugin
 import com.discord.api.commands.ApplicationCommandType
 
-import java.net.URLEncoder.encode
 import org.json.JSONObject
+import java.lang.StringBuilder
 
 @AliucordPlugin
 class Ip : Plugin() {
@@ -19,21 +19,32 @@ class Ip : Plugin() {
     private val LOGGER = Logger("Plugin - IP")
 
     override fun start(ctx: Context) {
-        val args = listOf(Utils.createCommandOption(
-                ApplicationCommandType.STRING,
-                "query",
-                "Enter IPv4/IPv6 address or domain name",
-                required = true
-        ))
+        val args = listOf(
+                Utils.createCommandOption(
+                        ApplicationCommandType.STRING,
+                        "query",
+                        "Enter IPv4/IPv6 address or domain name",
+                        required = true
+                ),
+                Utils.createCommandOption(
+                        ApplicationCommandType.BOOLEAN,
+                        "send",
+                        "Send to chat",
+                )
+        )
 
         commands.registerCommand("ip", "IP lookup", args) {
             val query = it.getRequiredString("query")
+            val shouldSend = it.getBoolOrDefault("send", false)
             try {
-                val url    = "http://ip-api.com/json/${query}"
+                val url = StringBuilder("http://ipwhois.app/json/$query?objects=")
+                        .append("success,ip,type,as,org,isp,continent,country,country_capital,city,region,")
+                        .append("country_phone,latitude,longitude,timezone,currency,completed_requests")
+                        .toString()
                 val result = Http.Request(url, "GET").execute().text()
                 val fmt    = JSONObject(result).toString(4)
                 val toSend = "```py\n${fmt}\n```"
-                CommandsAPI.CommandResult(toSend, null, false, "IP Info")
+                CommandsAPI.CommandResult(toSend, null, shouldSend, "IP Info")
             }
             catch (t: Throwable) {
                 LOGGER.error(t)
