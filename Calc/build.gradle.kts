6bf5b15b2d9a549f59e98b88639479b4371dd320
scrazzz <scrazzz@github.com>
2021-10-27 10:12:16 +0300

    Update Calc v2.0.0

--- Calc/build.gradle.kts
+++ Calc/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "1.0.0"
-description = "A simple calculator to do simple calculations"
+version = "2.0.0"
+description = "Calculate your question using Wolfram API."
 
 aliucord.changelog.set(
         """
+
+        # 2.0.0
+        * Added Wolfram API support
 
         # 1.0.0
         * Initial release
