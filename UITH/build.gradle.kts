80e15e33833aecb027acd8d34b62b81696e6963c
scrazzz <scrazzz@github.com>
2021-10-31 16:09:49 +0300

    incomplete - UITH

---
+++ UITH/build.gradle.kts
@@ -0,0 +1,11 @@
+version = "1.0.0"
+description = "Upload image to an image host."
+
+aliucord.changelog.set(
+        """
+
+        # 1.0.0
+        * Initial release
+
+        """.trimIndent()
+)
