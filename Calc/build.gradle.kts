6dfa3c318634b3ee5393430aa6a66bc1b0b147f1
scrazzz <scrazzz@github.com>
2021-11-11 18:31:47 +0300

    Fix crash when opening settings

--- Calc/build.gradle.kts
+++ Calc/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "2.0.0"
+version = "2.1.0"
 description = "Calculate your question using Wolfram API."
 
 aliucord.changelog.set(
         """
+
+        # 2.1.0
+        * Fix crash when opening settings
 
         # 2.0.0
         * Added Wolfram API support
