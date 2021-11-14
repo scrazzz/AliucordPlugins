79c0594a4277ad41dbcc8945d61380786d168e6b
scrazzz <70033559+scrazzz@users.noreply.github.com>
2021-11-14 23:53:09 +0300

    Update build.gradle.kts

--- NHentai/build.gradle.kts
+++ NHentai/build.gradle.kts
@@ -1,14 +1,14 @@
-version = "1.1.0"
+version = "2.1.0"
 description = "nhentai"
 
 aliucord.changelog.set(
         """
 
-        # 1.1.0
+        # 2.1.0
 	* Fixed wrong ID being shown in "search" command
 
-        # 1.0.0
-        * Initial release
+        # 2.0.1
+        * Initial release (Don't ask why)
 
         """.trimIndent()
 )
