c8e47dfe80aa9e2027b066ed7c90c3123b95e3e6
scrazzz <scrazzz@users.noreply.github.com>
2021-11-27 16:17:12 +0300

    Update Calc

---
+++ Calc/src/main/java/com/scruzism/plugins/result.kt
@@ -0,0 +1,11 @@
+package com.scruzism.plugins
+
+data class Subpods(val plaintext: String)
+
+data class Result(val queryresult: QueryResult) {
+    data class QueryResult(val inputstring: String, val pods: List<Pods>) {
+        data class Pods(
+                val subpods: List<Subpods> // main thing
+        )
+    }
+}
