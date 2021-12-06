3374c7eb74b1072b4f5975176b52917e180a8f5e
scrazzz <scrazzz@github.com>
2021-12-06 11:15:53 +0300

    Update unreleased plugin

--- UITH/build.gradle.kts
+++ UITH/build.gradle.kts
@@ -1,14 +1,11 @@
-version = "1.5.0"
+version = "1.5.1"
 description = "Upload image to an image host."
 
 aliucord.changelog.set(
         """
-
-        # 1.5.0
-        * Fix crash when opening settings
-
-        # 1.0.0
-        * Initial release
-
+        
+        1.5.1
+        * New settings added, check plugin settings info.\nUITH IS AN UNRELEASED PLUGIN. DON'T ASK FOR HELP WITH BUGS.
+        
         """.trimIndent()
 )
