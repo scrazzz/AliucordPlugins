2ddb9fcabf939c1f1be54ba11747d96983b7fd40
scrazzz <scrazzz@users.noreply.github.com>
2021-12-28 09:05:58 +0300

    Update

--- ChangeHypesquad/build.gradle.kts
+++ ChangeHypesquad/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "1.0.0"
+version = "1.1.0"
 description = "Change Hypesquad"
 
 aliucord.changelog.set(
         """
+
+        # 1.1.0
+        * Fix spelling mistakes
 
         # 1.0.0
         * Initial
