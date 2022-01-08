b80048286061cda427375f37ac9c1f368ba8acc8
scrazzz <scrazzz@github.com>
2022-01-08 10:44:05 +0300

    Update unfinished plugin

--- UITH/src/main/java/com/scruzism/plugins/UITH.kt
+++ UITH/src/main/java/com/scruzism/plugins/UITH.kt
@@ -1,139 +1,240 @@
 package com.scruzism.plugins
 
 import android.content.Context
-import com.aliucord.Constants
+import android.webkit.MimeTypeMap
 
 import com.aliucord.Http
-import com.aliucord.Utils
 import com.aliucord.Logger
-import com.aliucord.api.CommandsAPI.CommandResult
+import com.aliucord.Utils
 import com.aliucord.entities.Plugin
 import com.aliucord.annotations.AliucordPlugin
-import com.aliucord.api.SettingsAPI
 
+import com.aliucord.patcher.before
+import com.aliucord.utils.GsonUtils
+import com.aliucord.api.CommandsAPI.CommandResult
 import com.discord.api.commands.ApplicationCommandType
+import com.discord.widgets.chat.MessageContent
+import com.discord.widgets.chat.MessageManager
+import com.discord.widgets.chat.input.ChatInputViewModel
+import com.lytefast.flexinput.model.Attachment
+//import com.discord.api.message.LocalAttachment
+//import com.discord.utilities.attachments.AttachmentUtilsKt.toAttachment
 
-import java.io.File
-import java.util.UUID
+import com.google.gson.JsonSyntaxException
 import org.json.JSONObject
 
-private fun makeReq(host: String, name: String, file: File): MutableList<String> {
-    val mp = MultipartUtility(host, "UTF-8")
-    mp.addFilePart(name, file)
-    return mp.finish()
-}
+import java.io.File
+import java.io.IOException
+import java.lang.IndexOutOfBoundsException
+import java.util.regex.Pattern
 
-private fun uploadTo(host: String, f: File, settings: SettingsAPI): String{
-    return when (host) {
-        "0x0.st" -> {
-            return makeReq("https://0x0.st", "file", f)[0]
+
+private fun newUpload(file: File, data: Config, log: Logger): String {
+    val lock = Object()
+    val result = StringBuilder()
+
+    // thanks link
+    synchronized(lock) {
+        Utils.threadPool.execute {
+            try {
+                val params = mutableMapOf<String, Any>()
+                val resp = Http.Request("${data.RequestURL}", "POST")
+                if (data.Headers != null) {
+                    for ((k, v) in data.Headers!!.entries) {
+                        resp.setHeader(k, v)
+                    }
+                }
+
+                if (data.Arguments != null) {
+                    for ((k, v) in data.Arguments!!.entries) {
+                        params[k] = v
+                    }
+                }
+
+                params["${data.FileFormName}"] = file
+
+                result.append(resp.executeWithMultipartForm(params).text())
+            }
+            catch (ex: Throwable) {
+                if (ex is IOException) {
+                    log.debug("${ex.message} | ${ex.cause} | $ex | ${ex.printStackTrace()}")
+                }
+                log.error(ex)
+            }
+            finally {
+                synchronized(lock) {
+                    lock.notifyAll()
+                }
+            }
         }
-        "imgbb" -> {
-            val url = Http.QueryBuilder("https://api.imgbb.com/1/upload")
-                    .append("key", "93dd460db91461bbffa0ffcf1f6c475c")
-                    .append("name", UUID.randomUUID().toString().subSequence(0, 8).toString())
-                    .toString()
-            return JSONObject(makeReq(url, "image", f)[0]).getJSONObject("data").getString("url")
-        }
-        "sxcu" -> {
-            val subdomain = settings.getString("sxcuSubdomain", null)
-                    ?: return "You have not set an sxcu subdomain in settings"
-            val url = "https://$subdomain/api/files/create"
-            val multipart = MultipartUtility(url, "UTF-8")
-            multipart.addFilePart("file", f)
-            multipart.addFormField("noembed", "")
-            multipart.addFormField("og_properties", "{\"discord_hide_url\": true}")
-            return JSONObject("{${multipart.finish()[2].toString().trim().removeSuffix(",")}}").getString("url")
-        }
-        else -> "" // unreachable
+        lock.wait(9_000)
     }
+    log.debug(JSONObject(result.toString()).toString(4))
+    return result.toString()
 }
 
 @AliucordPlugin
 class UITH : Plugin() {
 
     private val LOG = Logger("UITH")
+    // https://github.com/TymanWasTaken/aliucord-plugins/blob/main/EncryptDMs/src/main/kotlin/tech/tyman/plugins/encryptdms/EncryptDMs.kt#L321-L326
+    private val textContentField = MessageContent::class.java.getDeclaredField("textContent").apply { isAccessible = true }
+    private fun MessageContent.set(text: String) = textContentField.set(this, text)
+
     init {
         settingsTab = SettingsTab(PluginSettings::class.java).withArgs(settings)
     }
 
-    /*
-    override fun load(context: Context?)
-    {
-        commands.unregisterAll()
-        Utils.showToast("Deleting UITH plugin because it's deprecated.\nInstall UTH", true)
-        LOG.info(Constants.PLUGINS_PATH)
-        File(Constants.PLUGINS_PATH + "/UITH.zip").delete()
-        File(Constants.BASE_PATH + "/UITH.json").delete()
-    }
-     */
-
-    override fun load(context: Context?) {
-        Utils.showToast("UITH is an unreleased plugin, Do NOT ask for help about bugs.", true)
-    }
-
     override fun start(ctx: Context) {
 
-        /* patch: automatically upload attachment to host */
-        // nvm it was harder than i thought, maybe i'll add it after learning how to patch
-
-        val defHost = settings.getString("defaultHost", "imgbb")
-
-        val hostChoices = listOf(
-                Utils.createCommandChoice("0x0.st", "0x0.st"),
-                Utils.createCommandChoice("imgbb", "imgbb"),
-                Utils.createCommandChoice("sxcu", "sxcu")
-        )
         val args = listOf(
                 Utils.createCommandOption(
-                        ApplicationCommandType.STRING,
-                        "host",
-                        "Choose an image upload host, Your default host: $defHost",
-                        choices = hostChoices
+                        ApplicationCommandType.SUBCOMMAND, "add", "Add sharex config",
+                        subCommandOptions = listOf(
+                                Utils.createCommandOption(
+                                        ApplicationCommandType.STRING,
+                                        "sharex",
+                                        "Add sharex config (paste the contents)",
+                                        required = true
+                                )
+                        )
                 ),
                 Utils.createCommandOption(
-                        ApplicationCommandType.STRING,
-                        "message",
-                        "An optional message to send"
+                        ApplicationCommandType.SUBCOMMAND, "current", "View current UITH settings"
                 ),
                 Utils.createCommandOption(
-                        ApplicationCommandType.BOOLEAN,
-                        "send",
-                        "send to chat"
+                        ApplicationCommandType.SUBCOMMAND, "disable", "Switch off plugin",
+                        subCommandOptions = listOf(
+                                Utils.createCommandOption(
+                                        ApplicationCommandType.BOOLEAN,
+                                        "disable",
+                                        required = true
+                                )
+                        )
                 )
         )
+        commands.registerCommand("uith", "Upload Image To Host", args) {
+            if (it.containsArg("add")) {
+                val config = try {
+                    GsonUtils.fromJson(it.getSubCommandArgs("add")?.get("sharex").toString(), Config::class.java)
+                }
+                catch (ex: JsonSyntaxException) {
+                    return@registerCommand CommandResult("Invalid sharex file data provided", null, false)
+                }
+                if (config?.RequestURL.isNullOrEmpty()) {
+                    return@registerCommand CommandResult("\"RequestURL\" must not be empty!", null, false)
+                }
+                if (config?.FileFormName.isNullOrEmpty()) {
+                    return@registerCommand CommandResult("\"FileFormName\" must not be empty!", null, false)
+                }
+                LOG.debug(config.toString())
+                settings.setString("sxcuConfig", it.getSubCommandArgs("add")?.get("sharex").toString())
+                return@registerCommand CommandResult("Set data successfully", null, false)
+            }
 
-        commands.registerCommand(
-                "uith",
-                "Upload Image To Host",
-                args
-        ) {
-            val host = it.getStringOrDefault("host", defHost)
-            val message = it.getStringOrDefault("message", "")
-            val send = it.getBoolOrDefault("send", settings.getBool("defaultSend", false))
+            if (it.containsArg("current")) {
+                val configData = settings.getString("sxcuConfig", null)
+                val configRegex = settings.getString("regex", null)
+                val settingsUploadAllAttachments = settings.getBool("uploadAllAttachments", false)
+                val settingsPluginOff = settings.getBool("pluginOff", false)
+                val sb = StringBuilder()
+                sb.append("sxcu config:```\n$configData\n```\n\n")
+                sb.append("regex:```\n$configRegex\n```\n\n")
+                sb.append("uploadAllAttachments: `$settingsUploadAllAttachments`\n")
+                sb.append("pluginOff: `$settingsPluginOff`")
+                return@registerCommand CommandResult(sb.toString(), null, false)
+            }
 
-            if (it.attachments.size > 1) { return@registerCommand CommandResult(
-                    "Plugin does not support multiple file uploads. Please select ONE file.",
-                    null, false, "UITH"
-            )}
-            if (it.attachments.size == 0) { return@registerCommand CommandResult(
-                    "You have not selected an attachment to upload.",
-                    null, false, "UITH"
-            )}
+            if (it.containsArg("disable")) {
+                val set = it.getSubCommandArgs("disable")?.get("disable").toString()
+                if (set.lowercase() == "true") settings.setString("pluginOff", set)
+                if (set.lowercase() == "false") settings.setString("pluginOff", set)
+                return@registerCommand CommandResult(
+                        "Plugin Disabled: ${settings.getString("pluginOff", false.toString())}", null, false
+                )
+            }
 
-            val file = File(it.attachments[0].data.toString())
-            val resp = uploadTo(host, file, settings)
-            LOG.info(resp)
-
-            val fmt = if (message.isEmpty()) { resp }
-                      else if (resp == "You have not set an sxcu subdomain in settings") {
-                          return@registerCommand CommandResult(resp, null, false, "UITH") }
-                      else { message + "\n" + resp }
-            it.attachments.clear()
-            CommandResult(fmt, null, send)
+            CommandResult("", null, false)
         }
+
+        patcher.before<ChatInputViewModel>(
+                "sendMessage",
+                Context::class.java,
+                MessageManager::class.java,
+                MessageContent::class.java,
+                List::class.java,
+                Boolean::class.javaPrimitiveType!!,
+                Function1::class.java
+        ) {
+            val context = it.args[0] as Context
+            val content = it.args[2] as MessageContent
+            val plainText = content.textContent
+            val attachments = (it.args[3] as List<Attachment<*>>).toMutableList()
+            val firstAttachment = try { attachments[0] } catch (t: IndexOutOfBoundsException) { return@before }
+
+            // Check if plugin is OFF
+            if (settings.getBool("pluginOff", false)) {
+                return@before
+            }
+
+            // Check if multiple attachments provided
+            if (!settings.getBool("pluginOff", false) == true) {
+                if (attachments.size > 1) {
+                    Utils.showToast("UITH: Multiple attachment uploads are not supported!", true)
+                    return@before
+                }
+            }
+
+            // Check file type and don't upload to host if `uploadAllAttachments` is false
+            // (There might be a better way to do this lol)
+            val mime = MimeTypeMap.getSingleton().getExtensionFromMimeType(context.getContentResolver().getType(firstAttachment.uri)) as String
+            if (mime !in arrayOf("png", "jpg", "jpeg", "webp")) {
+                if (settings.getBool("uploadAllAttachments", false) == false) {
+                    return@before
+                }
+            }
+
+            // Don't try to upload if no sxcu config given
+            val sxcuConfig = settings.getString("sxcuConfig", null)
+            if (sxcuConfig == null) {
+                LOG.debug("sxcuConfig not provided, skipping upload...")
+                return@before
+            }
+            // Upload to host
+            // but check if regex is set first before uploading
+            val re = if (settings.getString("regex", null).isNullOrEmpty()) {
+                LOG.debug("regex not set, skipping upload...")
+                return@before
+            } else {
+                settings.getString("regex", null).toRegex().toString()
+            }
+            val configData = GsonUtils.fromJson(sxcuConfig, Config::class.java)
+            //val file = toLocalAttachment(attachments[0])
+            val json = newUpload(File(firstAttachment.data.toString()), configData, LOG)
+
+            // match URL from regex
+            val url = try {
+                val matcher = Pattern.compile(re).matcher(json)
+                matcher.find()
+                matcher.group()
+            } catch (ex: Throwable) {
+                Utils.showToast("UITH: An error occurred, check debug logs", true)
+                LOG.error(ex)
+                return@before
+            }
+
+            // Send message with the URL received from host
+            content.set("$plainText\n$url")
+            it.args[2] = content
+            it.args[3] = emptyList<Attachment<*>>()
+            return@before
+        }
+
     }
 
-    override fun stop(ctx: Context) = commands.unregisterAll()
+    override fun stop(ctx: Context) {
+        patcher.unpatchAll()
+        commands.unregisterAll()
+    }
 
 }
--- UITH/src/main/java/com/scruzism/plugins/utils.kt
+++ UITH/src/main/java/com/scruzism/plugins/utils.kt
@@ -1,7 +1,15 @@
 package com.scruzism.plugins
 
-val defaultHosts = mapOf(
-        "imgbb" to "imgbb",
-        "0x0.st" to "0x0.st",
-        "sxcu" to "sxcu"
+// https://github.com/tsudoko/xshare/blob/master/app/src/main/java/re/flande/xshare/Uploader.kt#L13
+data class Config(
+        var Name: String?,
+        var DestinationType: String?,
+        var RequestType: String = "POST",  // imp
+        var RequestURL: String?,           // imp
+        var FileFormName: String?,         // imp
+        var Headers: Map<String, String>?,
+        var Arguments: Map<String, String>?,
+        // var RegexList: Array<String>?,
+        var ResponseType: String?,
+        var URL: String?
 )
