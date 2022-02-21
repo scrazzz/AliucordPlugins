86887011204bcf9531fbd7f9dc9a674fd564419d
scrazzz <scrazzz@users.noreply.github.com>
2022-02-21 12:46:55 +0300

    Improve new plugin

--- ScreenshotAPI/src/main/java/com/scruzism/plugins/ScreenshotAPI.kt
+++ ScreenshotAPI/src/main/java/com/scruzism/plugins/ScreenshotAPI.kt
@@ -8,7 +8,9 @@ import com.aliucord.Logger
 import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
 import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.MessageEmbedBuilder
 import com.discord.api.commands.ApplicationCommandType
+import com.discord.api.message.embed.MessageEmbed
 
 import java.net.URLEncoder
 
@@ -42,17 +44,22 @@ class ScreenshotAPI : Plugin() {
             val shouldSend = it.getBoolOrDefault("send", false)
             try {
                 val httpUrl = StringBuilder("https://shot.screenshotapi.net/screenshot?token=8YB357P-BNR4620-M3JRHZ7-7DJRFJJ")
-				    .append("&url=$url&output=json&file_type=png&block_ads=true&wait_for_event=load&ttl=15").toString()
-		log.debug(url)
-                val result = Http.Request(httpUrl, "GET").execute().json(APIResponse::class.java).screenshot
+				.append("&url=$url&output=json&file_type=png&block_ads=true&wait_for_event=load&ttl=15")
+				.toString()
+				log.debug(url)
+				val result = Http.Request(httpUrl, "GET").execute().json(APIResponse::class.java).screenshot
+
+				if (!shouldSend)
+				{
+					val embed = MessageEmbedBuilder().setRandomColor().setImage(result, null, 876, 1680).build()
+					return@registerCommand CommandsAPI.CommandResult(null, embed, false, "ScreenshotAPI")
+				}
 
                 CommandsAPI.CommandResult(result, null, shouldSend, "ScreenshotAPI")
             }
             catch (t: Throwable) {
                 log.error(t)
-                CommandsAPI.CommandResult(
-                        "An error occured. Check Debug Logs",
-                        null, false, "IP Info")
+                CommandsAPI.CommandResult("An error occured. Check Debug Logs", null, false)
             }
         }
 
